782b92563f222e3eb4508763198b9b05faae5f75
Thiago Spart <user000.000resu@gmail.com>
2021-06-20 15:50:35 -0300

    fist-commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from "./styles/global";
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
