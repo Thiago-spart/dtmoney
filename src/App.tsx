b3fcebfd624b75c705d4b7bb030a1c4f7cbc6b0c
Thiago Spart <user000.000resu@gmail.com>
2021-06-20 16:45:41 -0300

    Create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import { Header } from "./components/Header";
+
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
     <>
+      <Header />
       <GlobalStyles />
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
     </>
   );
 }
