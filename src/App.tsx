387dc82cf82f0263fe4deb4c522e5037d1bd8781
Thiago Moraes <user000.000resu@gmail.com>
2021-07-02 16:33:40 -0300

    CreateTransactionModal content

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import Modal from "react-modal";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { TransactionsTable } from "./components/TransactionsTable";
 
 import { GlobalStyles } from "./styles/global";
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
 export const App = () => {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
@@ -27,13 +28,10 @@ export const App = () => {
       <Dashboard />
       <TransactionsTable />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h1>hello mundo</h1>
-      </Modal>
-
+      />
       <GlobalStyles />
     </>
   );
