1238e7aa474002ca4761f6fb538c68e062adea41
Thiago Moraes <user000.000resu@gmail.com>
2021-07-06 17:57:29 -0300

    Transaction hooks

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
 import { FormEvent, useState } from "react";
+import { useTransactions } from "../../hooks/useTransactions";
 import Modal from "react-modal";
+
 import closeImg from "../../../assets/close.svg";
 import incomeImg from "../../../assets/income.svg";
 import outcomeImg from "../../../assets/outcome.svg";
-import { api } from "../../services/api";
 
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
@@ -16,22 +17,29 @@ export const NewTransactionModal = ({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) => {
+  const { createTransaction } = useTransactions();
+
   const [type, setType] = useState("deposit");
   const [title, setTitle] = useState("");
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState("");
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
+    await createTransaction({
       title,
-      value,
+      amount,
       category,
       type,
-    };
+    });
 
-    api.post("/transactions", data);
+    setTitle("");
+    setAmount(0);
+    setCategory("");
+    setType("deposit");
+
+    onRequestClose();
   }
 
   return (
@@ -61,8 +69,8 @@ export const NewTransactionModal = ({
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,13 +1,32 @@
-import { useContext } from "react";
+import { useTransactions } from "../../hooks/useTransactions";
+
 import incomeImg from "../../../assets/income.svg";
 import outcomeImg from "../../../assets/outcome.svg";
 import totalImg from "../../../assets/total.svg";
-import { TransactionsContext } from "../../TransactionsContext";
 
 import { Container } from "./styles";
 
 export const Summary = () => {
-  const transactions = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
 
   return (
     <Container>
@@ -15,7 +34,12 @@ export const Summary = () => {
         <header>
           <p>Entradas</p>
           <img src={incomeImg} alt="entradas" />
-          <strong>R$1.000</strong>
+          <strong>
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(summary.deposits)}
+          </strong>
         </header>
       </div>
 
@@ -23,7 +47,13 @@ export const Summary = () => {
         <header>
           <p>Saidas</p>
           <img src={outcomeImg} alt="saidas" />
-          <strong>- R$500</strong>
+          <strong>
+            -{" "}
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(summary.withdraws)}
+          </strong>
         </header>
       </div>
 
@@ -31,7 +61,12 @@ export const Summary = () => {
         <header>
           <p>Total</p>
           <img src={totalImg} alt="total" />
-          <strong>R$500</strong>
+          <strong>
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(summary.total)}
+          </strong>
         </header>
       </div>
     </Container>
